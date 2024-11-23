f070e0dd9dc9d97db69d2945afcfadd7640e9192
Ashpaque Shaikh <ashfaq664236@gmail.com>
2024-11-23 22:04:36 +0530

    Model Trainer Added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from mlproject import logger
 from mlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline 
 from mlproject.pipeline.stage_02_data_validation import DataValidtationTrainingPipeline
 from mlproject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from mlproject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -14,6 +15,8 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+
 STAGE_NAME = "Data Validation Stage"
 
 try:
@@ -25,12 +28,27 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+
 STAGE_NAME = "Data Transformation Stage"
 
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     data_validation = DataTransformationTrainingPipeline()
     data_validation.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Model Trainer Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
 except Exception as e:
     logger.exception(e)
---
+++ src/mlproject/pipeline/stage_04_model_trainer.py
@@ -0,0 +1,30 @@
+from mlproject.config.configuraion import ConfigurationManger
+from mlproject.components.modeL_trainer_04 import ModelTrainer
+from mlproject import logger
+
+
+STAGE_NAME = "Model Trainer Stage"
+
+
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+
+
+    def main(self):
+        config = ConfigurationManger()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        obj = ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
